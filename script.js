e2e05e8941563e9359c67acf5120d6dc3e7eee83
codingdestro <codingdestro@gmail.com>
2025-04-28 18:37:02 +0530

    feat: added modal for deleting items

--- script.js
+++ script.js
@@ -3,16 +3,24 @@ const input = document.querySelector(".add-input");
 
 let Items = [];
 
-function deleteBookmark(id) {
-  chrome.storage.local.remove(id, () => console.log(`remove ${id}`));
+function deleteBookmark(title, id) {
+  Items = Items.filter((item, idx) => {
+    console.log(item.title, idx);
+    return item.title != title || idx != id;
+  });
+  chrome.storage.local.set({ bookmarks: Items });
   loadSites();
 }
 
-function createDeleteBtn(id) {
-  const delete_btn = document.createElement("button");
-  delete_btn.classList.add(".delete-btn");
-  delete_btn.addEventListener("click", () => deleteBookmark(id));
-  return delete_btn;
+const modal = document.querySelector(".menu-modal");
+const buttons = document.querySelector(".menu").children;
+
+modal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+function openModal() {
+  modal.style.display = "flex";
 }
 
 function createImage(src) {
@@ -28,30 +36,32 @@ function createTitle(desc) {
   return title;
 }
 
-function createAnchor(id, url) {
+function createAnchor() {
   const span = document.createElement("span");
   return span;
 }
 
-function createElements(id,name, url, icon) {
-  const span = createAnchor(id, url);
+function createElements(id, name, url, icon) {
+  const span = createAnchor();
   const image = createImage(icon);
   span.appendChild(image);
   const title = createTitle(name);
 
   const li = document.createElement("li");
   li.addEventListener("contextmenu", (e) => {
-    e.preventDefault()
-    console.log(`id = ${id}`);
-    Items = Items.filter((item,idx)=> idx != id)
-    listItems()
+    e.preventDefault();
+    openModal();
+    buttons[0].addEventListener("click", (e) => {
+      e.stopPropagation();
+      deleteBookmark(name, id);
+      modal.style.display = "none";
+    });
   });
 
   li.addEventListener("click", () => {
     // If you want to open in new tab instead of current
     chrome.tabs.create({ url: url });
   });
-
 
   li.appendChild(span);
   li.appendChild(title);
@@ -85,7 +95,12 @@ function loadSites() {
 function listItems() {
   container.innerHTML = "";
   for (let i = 0; i < Items.length; i++) {
-    const element = createElements(i,Items[i].title, Items[i].url, Items[i].icon);
+    const element = createElements(
+      i,
+      Items[i].title,
+      Items[i].url,
+      Items[i].icon
+    );
     container.appendChild(element);
   }
 }
